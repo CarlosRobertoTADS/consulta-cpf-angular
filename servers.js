7969a830b0b4905e31f8d395039f8d00dc36d513
Carlos Maciel <carlosbeto1996@gmail.com>
2022-06-28 16:46:34 -0400

    add server

---
+++ servers.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/mirante-teste-angular'));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(__dirname + '/dist/mirante-teste-angular/index.html'));
+});
+
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
